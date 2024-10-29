ce332a282361bd49710542fdb38976404e13d3e9
marcelin243 <160675042+marcelin243@users.noreply.github.com>
2024-10-29 22:15:13 +0200

    Update settings.py

--- pylint_demo/pylint_demo/settings.py
+++ pylint_demo/pylint_demo/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kt5x9@fm9$pck_-qz+u(raft^+3v59nkuts0q-_*_x=1_bd!k^'
+# SECRET_KEY = 'django-insecure-kt5x9@fm9$pck_-qz+u(raft^+3v59nkuts0q-_*_x=1_bd!k^'
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
