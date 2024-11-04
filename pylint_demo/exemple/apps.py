4a7742d4d4bdd4a7abf3877b5d302986c42b67eb
marcelin243 <160675042+marcelin243@users.noreply.github.com>
2024-11-04 11:23:20 +0200

    eee

--- pylint_demo/exemple/apps.py
+++ pylint_demo/exemple/apps.py
@@ -1,6 +1,13 @@
+"""
+Module de configuration des applications Django pour l'application Exemple.
+"""
+
 from django.apps import AppConfig
 
-
 class ExempleConfig(AppConfig):
+    """
+    Configuration de l'application Exemple.
+    """
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'exemple'
--- pylint_demo/tests.py
+++ pylint_demo/tests.py
@@ -1,10 +1,22 @@
+"""
+Module de tests combinés pour l'application.
+"""
+
+import unittest  # Import standard
+
 from django.test import TestCase
-from exemple.tests import UserAPITestCase  # Assurez-vous que le chemin est correct
-import unittest
+from exemple.tests import UserAPITestCase  # Import tiers
 
 class CombinedTests(TestCase):
+    """
+    Classe de tests combinés pour exécuter les tests de UserAPITestCase.
+    """
+
     def test_user_app_tests(self):
-        # Créez une suite de tests et ajoutez-y les tests de UtilisateurAPIViewTests
+        """
+        Exécute les tests de UserAPITestCase et vérifie si tous les tests ont réussi.
+        """
+        # Créez une suite de tests et ajoutez-y les tests de UserAPITestCase
         suite = unittest.TestSuite()
         suite.addTest(unittest.makeSuite(UserAPITestCase))
 
@@ -12,4 +24,6 @@ class CombinedTests(TestCase):
         result = unittest.TextTestRunner().run(suite)
 
         # Vérifiez si tous les tests ont réussi
-        self.assertTrue(result.wasSuccessful(), "Les tests  ont échoué.")
+        self.assertTrue(result.wasSuccessful(), "Les tests ont échoué.")
+
+# Assurez-vous d'avoir une nouvelle ligne ici
